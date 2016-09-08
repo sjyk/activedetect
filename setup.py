4bd0785ae6d807b1e6f3892c8e4bff437022c735
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-09-07 23:43:40 -0700

    Added in the dependencies as well

--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@ from setuptools import setup, find_packages
 setup(
   name = 'activedetect',
   packages = find_packages(), # this must be the same as the name above
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'A Library For Error Detection For Predictive Analytics',
   author = 'Sanjay Krishnan and Eugene Wu',
   author_email = 'sanjay@eecs.berkeley.edu',
   url = 'https://github.com/sjyk/activedetect/', # use the URL to the github repo
-  download_url = 'https://github.com/sjyk/activedetect/tarball/0.1.1', 
+  download_url = 'https://github.com/sjyk/activedetect/tarball/0.1.3', 
   keywords = ['error', 'detection', 'cleaning'], # arbitrary keywords
   classifiers = [],
+  install_requires=[
+          'numpy',
+          'sklearn',
+          'gensim',
+          'usaddress',
+          'scipy'
+      ]
 )
