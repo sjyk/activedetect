183072a44dd42185e9e374da740ff0ee5a229bdd
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-09-07 23:27:28 -0700

    Updated the setup script, seems to work

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(
   name = 'activedetect',
   packages = find_packages(), # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'A Library For Error Detection For Predictive Analytics',
   author = 'Sanjay Krishnan and Eugene Wu',
   author_email = 'sanjay@eecs.berkeley.edu',
   url = 'https://github.com/sjyk/activedetect/', # use the URL to the github repo
-  download_url = 'https://github.com/sjyk/activedetect/tarball/0.1', 
+  download_url = 'https://github.com/sjyk/activedetect/tarball/0.1.1', 
   keywords = ['error', 'detection', 'cleaning'], # arbitrary keywords
   classifiers = [],
 )
