b25e12f7b830e02833dc6ea38e4c625860bdcd6e
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-09-07 23:55:47 -0700

    Incrementing the version number

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
   name = 'activedetect',
   packages = find_packages(), # this must be the same as the name above
-  version = '0.1.3',
+  version = '0.1.4-1',
   description = 'A Library For Error Detection For Predictive Analytics',
   author = 'Sanjay Krishnan and Eugene Wu',
   author_email = 'sanjay@eecs.berkeley.edu',
   url = 'https://github.com/sjyk/activedetect/', # use the URL to the github repo
-  download_url = 'https://github.com/sjyk/activedetect/tarball/0.1.3', 
+  download_url = 'https://github.com/sjyk/activedetect/tarball/0.1.4-1', 
   keywords = ['error', 'detection', 'cleaning'], # arbitrary keywords
   classifiers = [],
   install_requires=[
