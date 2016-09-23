577b0e854778bfbc5eecdd1b2e8db73f6b83d928
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-09-22 22:43:30 -0700

    Added a new error module and a new benchark

---
+++ activedetect/error_detectors/PuncErrorModule.py
@@ -0,0 +1,66 @@
+"""
+Detects attributes that are only punctuation,
+whitespace, etc
+"""
+import numpy as np
+import re
+from ErrorModule import ErrorModule
+
+class PuncErrorModule(ErrorModule):	
+
+	"""
+	Returns the subset of a records that are potentially
+	erroneous
+
+	use dictionary instead
+
+	O(N^2): N distinct values
+	"""
+	def predict(self, strings):
+
+		incorpus = set(strings)
+		error = set()
+
+		for s in set(strings):
+
+			sstrip = re.sub(r'\W+', '', s.lower())
+
+			cleaned_string = sstrip.lower().strip()
+
+			if len(cleaned_string) == 0:
+				error.add(s)
+				incorpus.remove(s)
+
+		return list(error), list(incorpus)
+
+
+	"""
+	Turns an error set into a set of records
+	"""
+	def getRecordSet(self, errors, dataset, col):
+
+		indices = []
+		erecords = []
+		eset = set(errors)
+
+		for i,d in enumerate(dataset):
+
+			val = d[col]
+
+			if val in eset:
+				indices.append(i)
+				erecords.append(d)
+				
+		return erecords, indices
+
+	def desc(self):
+		return "An attribute was found with no alpha numeric characeters"
+
+
+	def availTypes(self):
+		return ['categorical', 'string']
+
+
+
+
+		
